87e930555c030991257e9495d3d269e805d8e3f2
servosdesign <servos.design0@gmail.com>
2022-09-25 18:12:28 -0500

    format

--- print-ascii-ranged/print-ascii-ranged.asm
+++ print-ascii-ranged/print-ascii-ranged.asm
@@ -8,8 +8,9 @@
         msg5:   .asciiz " are: "
         newLine: .asciiz "\n"
 
-        upperRange: .asciiz " "         # Storing upperRange user entered character here
         lowerRange: .asciiz " "         # Storing lowerRange user entered character here
+        upperRange: .asciiz " "         # Storing upperRange user entered character here
+        
 
         .text
         .globl main
@@ -21,22 +22,22 @@ main:
         li      $v0, 12                 # read_character
         syscall
 
-        la $t0, upperRange              # Store the upperRange value in the user data segment
-        sb $v0, 0($t0)
+        la      $t0, lowerRange         # Store the lowerRange value in the user data segment
+        sb      $v0, 0($t0)
 
-        li $v0, 4                       # Enter newLine
-        la $a0, newLine
+        li      $v0, 4                  # Enter newLine
+        la      $a0, newLine
         syscall
 
         addi    $v0, $0, 4
 	      la      $a0, msg2               # Prompt message msg2
 	      syscall	
-                                        # Take user upper range character input
+                                        # Take user lowerRange character input
         li      $v0, 12                 # read_character
         syscall
 
-        la $t0, lowerRange              # Store the lowerRange value in the user data segment
-        sb $v0, 0($t0)
+        la      $t0, upperRange         # Store the upperRange value in the user data segment
+        sb      $v0, 0($t0)
 
         li      $v0, 4                  # Enter newLine
         la      $a0, newLine
@@ -46,26 +47,26 @@ main:
 	      la      $a0, msg3               # Prompt message msg3
 	      syscall	
 
-        li      $v0, 4                  # Display user entered upperRange character
-        la      $a0, upperRange
+        li      $v0, 4                  # Display user entered lowerRange character
+        la      $a0, lowerRange
         syscall
 
         addi    $v0, $0, 4
 	      la      $a0, msg4               # Prompt message msg4
 	      syscall
 
-        li $v0, 4                       # Display user entered lowerRange character
-        la $a0, lowerRange
+        li      $v0, 4                  # Display user entered upperRange character
+        la      $a0, upperRange
         syscall
 
         addi    $v0, $0, 4
 	      la      $a0, msg5               # Prompt message msg5
 	      syscall
       
-        lb      $t2, upperRange         # c = lowerRange 
+        lb      $t2, lowerRange         # c = lowerRange 
         addiu   $t2, $t2, 1             # c = lowerRange + 1
         
-        lb      $t3, lowerRange         # load upperRange into $t1                     
+        lb      $t3, upperRange         # load upperRange into $t1                     
 
 loop: 
         blt     $t2, $t3, inner         # c < upperRange
@@ -76,7 +77,7 @@ inner:
         syscall                         
 
         addi    $t2, $t2, 1             # c = c + 1
-        beq     $t2, $t3, exit
+        beq     $t2, $t3, exit          # when c = upperRange go to exit
         j loop
 
 exit: 
