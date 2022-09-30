0be3f1b019c282c2828db3d5c30de22788a7e040
servosdesign <servos.design0@gmail.com>
2022-09-30 18:25:03 -0500

    :df

--- nested-loops/nested-loops.asm
+++ nested-loops/nested-loops.asm
@@ -5,50 +5,81 @@ msg1:   .asciiz "Please enter the number of columns: "
 space: .asciiz " "
 newline: .asciiz "\n"
 
-n: .word 0                                  # Storing lowerRange user entered character here
+clear:    .float   0.0
+n: .word 0                                          # Storing lowerRange user entered character here
 
 .text
 .globl main
+
 main:
     addi    $v0,    $0,         4
-    la      $a0,    msg1                    # Prompt message msg1
+    la      $a0,    msg1                            # Prompt message msg1
     syscall 
 
-    li      $v0,    5
+    li      $v0,    5                               # Taking user input
     syscall 
-    move    $t0,    $v0
+    move    $t0,    $v0                             # Storing it for first half
+    move    $t6,    $t0                             # Storing it for second half
 
-    add     $s1,    $zero,      $t0         # loop n times
-    addi    $t0,    $zero,      1           # i = 1
+    add     $s1,    $zero,      $t0                 # Loop n times
+    addi    $t0,    $zero,      1                   # i = 1
 
 outerloop:
-    addi    $t1,    $zero,      1           # j = 1
+    addi    $t1,    $zero,      1                   # j = 1
 
 innerloop:
-                                            # printing the counter
-    add     $a0,    $zero,      $t1         # $a0 = j
+
+    add     $a0,    $zero,      $t1                 # $a0 = j
     li      $v0,    1
     syscall 
 
-# printing the space
-    la      $a0,    space
+    la      $a0,    space                           # Space
     li      $v0,    4
     syscall 
 
-# increment j
-    addi    $t1,    $t1,        1
-                                            # if j <= i, goto innerloop
-    ble     $t1,    $t0,        innerloop
+    addi    $t1,    $t1,        1                   # Increment j
 
-# printing the newline
-    la      $a0,    newline
+    ble     $t1,    $t0,        innerloop           # If j <= i, jump to innerloop
+
+    la      $a0,    newline                         # Newline
     li      $v0,    4
     syscall 
 
-# increment i
-    addi    $t0,    $t0,        1
-                                            # if i <= 4, goto outerloop
+    addi    $t0,    $t0,        1                   # increment i
+                                                    # if i <= 4, goto outerloop
     ble     $t0,    $s1,        outerloop
+
+    li      $t3,    0
+    li      $t4,    0
+
+SecondOuterLoop:
+    addi    $t6,    $zero,      1                   # i = 1
+
+    add     $s1,    $zero,      $t6                 # Loop n times
+
+    addi    $t1,    $zero,      1                   # j = 1
+
+SecondInnerLoop:
+    add     $a0,    $zero,      $t1                 # $a0 = j
+    li      $v0,    1
+    syscall 
+
+    la      $a0,    space                           # Space
+    li      $v0,    4
+    syscall 
+
+    addi    $t1,    $t1,        1                   # Increment j
+
+    ble     $t1,    $t0,        SecondInnerLoop     # If j <= i, jump to innerloop
+
+    la      $a0,    newline                         # Newline
+    li      $v0,    4
+    syscall 
+
+    addi    $t6,    $t6,        1                   # increment i
+                                                    # if i <= 4, goto outerloop
+    ble     $t6,    $s1,        SecondOuterLoop
+
 exit:
     li      $v0,    10
     syscall 
